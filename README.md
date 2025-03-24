ae3f53df4cbf0b7a51e693b472648a4f7d2861ff
DDXDB <DDXDB@outlook.com>
2025-03-25 03:46:48 +0800

    Prototype code

---
+++ README.md
@@ -0,0 +1,10 @@
+<p align="center">
+ <img width="250px" src="https://raw.githubusercontent.com/DDXDB/XAL/refs/heads/main/docs/XAL%20Logo.png" align="center" alt="GitHub Readme Stats" />
+ <h1 align="center">Xbox Advanced Launcher
+</h1>
+
+<p align="center">Currently, the Xbox PC App lacks the necessary Launch Parameter management
+
+<p align="center">This project is dedicated to providing unified Launch Parameter management for Xbox PC apps
+
+</p>
---
+++ src/XAL/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace XAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnAddDirectory = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnAddDirectory
+            // 
+            btnAddDirectory.Location = new Point(1080, 12);
+            btnAddDirectory.Name = "btnAddDirectory";
+            btnAddDirectory.Size = new Size(170, 61);
+            btnAddDirectory.TabIndex = 0;
+            btnAddDirectory.Text = "添加XboxGames目录";
+            btnAddDirectory.UseVisualStyleBackColor = true;
+            btnAddDirectory.Click += btnAddDirectory_Click;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.Location = new Point(12, 79);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1238, 579);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, -32);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(138, 131);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1262, 673);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(btnAddDirectory);
+            Controls.Add(pictureBox1);
+            MinimumSize = new Size(800, 500);
+            Name = "Form1";
+            Text = "游戏启动器";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddDirectory;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private PictureBox pictureBox1;
+    }
+}
